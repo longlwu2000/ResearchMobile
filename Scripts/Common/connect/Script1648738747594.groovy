cf8caa4d4f8e700c03c21e8eb4ff2d687079be56
longlwu2000 <99626338+longlwu2000@users.noreply.github.com>
2022-04-05 10:30:53 +0700

    change apk ezl

--- Scripts/Common/connect/Script1648738747594.groovy
+++ Scripts/Common/connect/Script1648738747594.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('D:\\Dev\\apk\\app-debug1.apk', true)
+Mobile.startApplication('D:\\Dev\\apk\\ezl.apk', true)
 
 Mobile.delay(2)
 
